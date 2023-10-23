1360d7d68151cf8ca9f3f571dcb2baa2a83fa562
rodentskie <rodentskie@gmail.com>
2023-10-24 04:56:47 +0800

    Initial commit

---
+++ apps/practera-badges-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'practera-badges-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/practera-badges-e2e',
+};
