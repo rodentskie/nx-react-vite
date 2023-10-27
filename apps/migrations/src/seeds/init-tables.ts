66dc64348d6527c04a4d77e2e4bce885de0e826b
rodentskie <rodentskie@gmail.com>
2023-10-28 05:27:01 +0800

    feat: create table

--- apps/migrations/src/seeds/init-tables.ts
+++ apps/migrations/src/seeds/init-tables.ts
@@ -3,14 +3,40 @@ export default async function () {
   const dynamoDB = dbInstance();
 
   const tables = ['migrations', 'certificates', 'badges'];
-  dynamoDB.listTables({}, (err, data) => {
-    if (err) {
-      console.error(
-        'Unable to list tables. Error JSON:',
-        JSON.stringify(err, null, 2)
-      );
-    } else {
-      console.log('Table names:', data.TableNames);
-    }
+
+  tables.forEach((data) => {
+    const params = {
+      TableName: data,
+      KeySchema: [
+        {
+          AttributeName: 'id',
+          KeyType: 'HASH',
+        },
+      ],
+      AttributeDefinitions: [
+        {
+          AttributeName: 'id',
+          AttributeType: 'S',
+        },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5,
+      },
+    };
+
+    dynamoDB.createTable(params, (err, data) => {
+      if (err) {
+        console.error(
+          'Unable to create table. Error JSON:',
+          JSON.stringify(err, null, 2)
+        );
+      } else {
+        console.log(
+          'Created table. Table description JSON:',
+          JSON.stringify(data, null, 2)
+        );
+      }
+    });
   });
 }
